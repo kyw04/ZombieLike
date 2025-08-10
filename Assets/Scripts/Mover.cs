a8b6541184c774d74a3a76c6f16343cd701a3787
kyw04 <85551017+kyw04@users.noreply.github.com>
2025-08-10 16:34:47 +0900

    init

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mover : MonoBehaviour
+{
+    private PlayerInput input;
+    private InputAction moveAction;
+
+    [SerializeField] private float speed = 7.5f;
+    
+    private void Awake()
+    {
+        input = GetComponent<PlayerInput>();
+        moveAction = input.actions["Move"];
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 direction = moveAction.ReadValue<Vector2>();
+        Debug.Log(direction);
+        transform.Translate(direction * (speed * Time.deltaTime));
+    }
+}
