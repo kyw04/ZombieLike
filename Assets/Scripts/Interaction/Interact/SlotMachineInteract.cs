6f9fa327b08d63c6e2f18ceeb3edf0b40276237c
kyw04 <85551017+kyw04@users.noreply.github.com>
2025-09-19 00:57:36 +0900

    update: slot machine
    아직 오류 있어서 이쁘게 돌아가게 수정 해야 함

--- Assets/Scripts/Interaction/Interact/SlotMachineInteract.cs
+++ Assets/Scripts/Interaction/Interact/SlotMachineInteract.cs
@@ -1,6 +1,4 @@
-using System;
 using Interaction;
-using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -9,6 +7,8 @@ public class SlotMachineInteract : MonoBehaviour, IInteractable
     public SpriteRenderer[] reels;
     private Texture2D[] slots = new Texture2D[3];
     private int[] correctCount;
+    private int[] placeIndex;
+    private float[] currentSpeed;
     public bool isPlayed;
     public int placeCount;
     
@@ -28,6 +28,8 @@ public class SlotMachineInteract : MonoBehaviour, IInteractable
     private void Awake()
     {
         correctCount = new int[textures.Length];
+        placeIndex = new int[reels.Length];
+        currentSpeed = new float[reels.Length];
 
         int width = textures[0].width;
         int height = textures[0].height;
@@ -58,6 +60,7 @@ public class SlotMachineInteract : MonoBehaviour, IInteractable
         
         for (int i = placeCount; i < reels.Length; i++)
         {
+            currentSpeed[i] = speed;
             reels[i].material.SetTexture("_TexArray", texArray);
             reels[i].material.SetInt("_TexCount", textures.Length);
         }
@@ -67,11 +70,34 @@ public class SlotMachineInteract : MonoBehaviour, IInteractable
     {
         if (isPlayed)
         {
-            for (int i = placeCount; i < reels.Length; i++)
+            for (int i = 0; i < reels.Length; i++)
             {
                 float centerIndex = reels[i].material.GetFloat("_CenterIndex");
-                centerIndex = (centerIndex + Time.deltaTime * speed) % textures.Length;
+                centerIndex = (centerIndex + Time.deltaTime * currentSpeed[i]) % textures.Length;
                 reels[i].material.SetFloat("_CenterIndex", centerIndex);
+
+                if (i < placeCount)
+                {
+                    if (currentSpeed[i] < 3.0f)
+                    {
+                        float min = Mathf.Abs(placeIndex[i] - (int)centerIndex);
+                        min = Mathf.Min(min, Mathf.Abs(placeIndex[i] - ((int)centerIndex + textures.Length)));
+                        currentSpeed[i] = speed * min / textures.Length;
+
+                        if (min == 0)
+                        {
+                            reels[i].material.SetFloat("_CenterIndex", placeIndex[i]);
+                        }
+                    }
+                    else if (0 < currentSpeed[i])
+                    {
+                        currentSpeed[i] -= Time.deltaTime * speed * 0.5f;
+                    }
+                    else if (0 > currentSpeed[i])
+                    {
+                        currentSpeed[i] = 0;
+                    }
+                }
             }
         }
     }
@@ -86,17 +112,16 @@ public class SlotMachineInteract : MonoBehaviour, IInteractable
             int max = 0, maxIndex = 0;
             int index = Random.Range(0, textures.Length);
             
-            Debug.Log(index);
+            placeIndex[placeCount] = index;
             slots[placeCount] = textures[index];
             correctCount[index]++;
-            reels[placeCount].material.SetFloat("_CenterIndex", index);
         
             if (max < correctCount[index])
             {
                 max = correctCount[index];
                 maxIndex = index;
             }
-            Debug.Log($"maxIndex: {maxIndex}, max: {max}");
+            // Debug.Log($"maxIndex: {maxIndex}, max: {max}");
             placeCount++;
         }
         isPlayed = true;
@@ -105,6 +130,9 @@ public class SlotMachineInteract : MonoBehaviour, IInteractable
     
     private void End()
     {
+        for (int i = 0; i < reels.Length; i++)
+            currentSpeed[i] = speed;
+        
         correctCount = new int[textures.Length];
         isPlayed = false;
         placeCount = 0;
